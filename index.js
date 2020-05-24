cb9d00801535e40047b0a0ec47a9564b70f21e8e
Try Wijono <3wijono@gmail.com>
2020-05-24 14:48:47 +0800

    updated icons and added update and save button

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-
 const todos = document.querySelector('#todo');
 const addButton = document.querySelector('#addButton');
 const tasks = document.querySelector('#tasks');
 const tasksChildren = tasks.children;
 
+
 const renderNewHTML = () => {
-    const grandNewDiv = document.createElement('div');
-    grandNewDiv.classList.add('columns');
+  const grandNewDiv = document.createElement('div');
+  grandNewDiv.classList.add('columns');
 
-    const checkBox = document.createElement('input');
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.classList.add('level-item');
-    grandNewDiv.append(checkBox);
-    
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML = `${todos.value}`;
-    newDiv.classList.add('column');
-    newDiv.classList.add('box');
-    newDiv.classList.add('is-four-fifths');
-    grandNewDiv.append(newDiv);
-    
-    const input = document.createElement('button');
-    // input.classList.add('column');
-    input.classList.add('delete');
-    input.classList.add('level-item');
-    grandNewDiv.append(input);
-    document.querySelector('#tasks').append(grandNewDiv);
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.classList.add('level-item');
+  grandNewDiv.append(checkBox);
 
-    checkBox.addEventListener('change', () => {
-        if (checkBox.checked) {newDiv.classList.add('strikethrough')}
-        else {newDiv.classList.remove('strikethrough')}
-        console.log(1);
-    });
-    input.addEventListener('click', () => {
-        grandNewDiv.remove();
-    });
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = `${todos.value}`;
+  newDiv.classList.add('column');
+  newDiv.classList.add('box');
+  newDiv.classList.add('is-four-fifths');
+  grandNewDiv.append(newDiv);
+
+  const updateBtn = document.createElement('button');
+  updateBtn.classList.add('button');
+  updateBtn.innerHTML = '<i class="fas fa-edit"></i>';
+  grandNewDiv.append(updateBtn);
+
+  const saveEditBtn = document.createElement('button');
+  saveEditBtn.classList.add('button');
+  saveEditBtn.classList.add('is-hidden');
+  saveEditBtn.innerHTML = '<i class="fas fa-save"></i>';
+  grandNewDiv.append(saveEditBtn);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('button');
+  deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  grandNewDiv.append(deleteBtn);
+
+  document.querySelector('#tasks').append(grandNewDiv);
+
+  checkBox.addEventListener('change', () => {
+    if (checkBox.checked) {newDiv.classList.add('strikethrough')}
+    else {newDiv.classList.remove('strikethrough')}
+  });
+
+  updateBtn.addEventListener('click', () => {
+    saveEditBtn.classList.remove('is-hidden');
+    updateBtn.classList.add('is-hidden');
+  });
+
+  saveEditBtn.addEventListener('click', () => {
+    saveEditBtn.classList.add('is-hidden');
+    updateBtn.classList.remove('is-hidden');
+  });
+
+  deleteBtn.addEventListener('click', () => {
+    grandNewDiv.remove();
+  });
 };
 
 
 
 const addItem = (todos) => {
-    renderNewHTML();
-    todos.value = "";
-
+  renderNewHTML();
+  todos.value = "";
 };
 
 
 addButton.addEventListener('click', () => {
-    addItem(todos);
+  addItem(todos);
 });
